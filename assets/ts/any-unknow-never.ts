71dd020af6bb3e9c6589bdc2d1c30d8a2ca05564
RoAlencar <roalencarx@gmail.com>
2022-07-06 17:16:00 -0300

    Como funciona o tipo never e o comando throw

--- assets/ts/any-unknow-never.ts
+++ assets/ts/any-unknow-never.ts
@@ -13,8 +13,13 @@ unknowValor = true;
 unknowValor = "vai sim"
 
 let stringTest2: string = "agora vai";
-stringTest2 = unknowValor;
 
 if(typeof unknowValor === 'string'){
     stringTest2 = unknowValor;
 }
+
+function jogaErro(erro: string, codigo: number): never{
+    throw{error: erro, code: codigo}
+}
+
+jogaErro('deu erro', 500);
