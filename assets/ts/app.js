5905820e8a4e491d696f73b7e2ac088dce7ec194
RoAlencar <roalencarx@gmail.com>
2022-07-06 17:06:08 -0300

    incluindo callback em funções

--- assets/ts/app.js
+++ assets/ts/app.js
@@ -1,9 +1,1 @@
 "use strict";
-function somarValoresNumericos(numero1, numero2) {
-    return numero1 + numero2;
-}
-console.log(somarValoresNumericos(1, 3));
-//Funções sem retorno são void
-function pritaValoresNumericos(numero1, numero2) {
-    console.log(numero1 + numero2);
-}
