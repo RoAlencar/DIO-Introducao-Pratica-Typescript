6a0b9897399005eb960852fe0836a7370ec8c940
RoAlencar <roalencarx@gmail.com>
2022-07-06 16:57:00 -0300

    Quando usar o tipo void em funções

--- assets/ts/app.js
+++ assets/ts/app.js
@@ -3,3 +3,7 @@ function somarValoresNumericos(numero1, numero2) {
     return numero1 + numero2;
 }
 console.log(somarValoresNumericos(1, 3));
+//Funções sem retorno são void
+function pritaValoresNumericos(numero1, numero2) {
+    console.log(numero1 + numero2);
+}
