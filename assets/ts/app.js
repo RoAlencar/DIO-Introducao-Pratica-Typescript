c767df4803aec216a95de80ca6ec54ff4efdb0f9
RoAlencar <roalencarx@gmail.com>
2022-07-06 17:11:48 -0300

    Any vs Unknow

--- assets/ts/app.js
+++ assets/ts/app.js
@@ -1,1 +1,17 @@
 "use strict";
+let anyEstaDeVolta;
+anyEstaDeVolta = 3;
+anyEstaDeVolta = 'teste';
+anyEstaDeVolta = 5;
+let stringTest = 'verificar';
+stringTest = anyEstaDeVolta;
+let unknowValor;
+unknowValor = 3;
+unknowValor = 'UEPA';
+unknowValor = true;
+unknowValor = "vai sim";
+let stringTest2 = "agora vai";
+stringTest2 = unknowValor;
+if (typeof unknowValor === 'string') {
+    stringTest2 = unknowValor;
+}
